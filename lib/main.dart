f70d8acbbf13388d6d930c026c05e1bded31f461
abdulaziz-alhashedi <ezz.alhashedi@gmail.com>
2026-02-22 17:22:00 +0300

    ls5: Layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,114 @@
 import 'package:flutter/material.dart';
+import 'game.dart';
 
 void main() {
   runApp(const MainApp());
 }
+
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.centerLeft,
+            child: Text('Birdle'),
+          ),
+        ),
+        body: Center(child: GamePage()),
+      ),
+    );
+  }
+}
+
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: Replace with screen contents
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (var guess in _game.guesses) ...[
+            Row(
+              spacing: 5.0,
+              children: [
+                 for (var letter in guess)
+                    Tile(letter.char, letter.type),
+              ],
+            ),
+          ],
+          // Add children next.
+        ],
+      ),
+    );
+
+    return Container();
+  }
+}
+
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           // child: Text(' عبدالعزيز'),
+//           // green
+//           child:  Row(
+//             children: [
+//               Tile('A', HitType.hit),
+//               Tile('A', HitType.miss),
+//               Tile('A', HitType.partial), 
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter;
+  final HitType hitType;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        border: Border.all(color: Colors.grey.shade300),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.grey,
+          _ => Colors.white,
+        },
+      ),
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
         ),
       ),
     );
   }
 }
+
